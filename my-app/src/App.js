6e990ded2dc2959c4a56b33e0324aee7739bd697
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-05-03 10:08:23 -0500

    starter files w/ deletes

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+    <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+
+    return (
+        <header className="site-header">
+            <div className="container">
+                <NavLink to="index.html" id="branding">
+                    <img src="../images/logo.png" alt="" className="logo"></img>
+                    <div className="logo-copy">
+                        <h1 className="site-title">Company Name</h1>
+                        <small className="site-description">Tagline goes here</small>
+                    </div>
+                </NavLink>
+
+                <div className="main-navigation">
+                    <button type="button" className="menu-toggle"><i className="fa fa-bars"></i></button>
+                    <ul className="menu">
+                        <li className="menu-item current-menu-item"><NavLink to="index.html">Home</NavLink></li>
+                        <li className="menu-item"><NavLink to="about.html">About</NavLink></li>
+                        <li className="menu-item"><NavLink to="review.html">Movie reviews</NavLink></li>
+                        <li className="menu-item"><NavLink to="joinus.html">Join us</NavLink></li>
+                        <li className="menu-item"><NavLink to="contact.html">Contact</NavLink></li>
+                    </ul>
+
+                    <form action="#" className="search-form">
+                        <input type="text" placeholder="Search..."></input>
+                        <button><i className="fa fa-search"></i></button>
+                    </form>
+                </div>
+
+                <div className="mobile-navigation"></div>
+        </div>
+    </header>
+    );
+}
+ 
+export default Header;
