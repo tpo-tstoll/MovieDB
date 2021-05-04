c63f416529a9101afef88352ec3eba0e4ff4aec7
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-05-04 17:49:15 -0500

    stlying

--- my-app/src/Components/SearchResults.jsx
+++ my-app/src/Components/SearchResults.jsx
@@ -16,8 +16,8 @@ const SearchResults = () => {
                         <div className="slider">
                         <h2>Search Results:</h2>
                                 {value.searchResults.map(movie => {
-                                   return <ul className="slides"> <li className='col-md-4' key={movie.id}><NavLink to={`/${movie.id}`}><img src={`https://image.tmdb.org/t/p/original${movie.image}`} alt={movie.title}></img></NavLink></li>
-                                   <li className="col-md-3 col-centered" top='50%'><NavLink to={`/${movie.id}`}><h1 className='maintitle'>{movie.title}</h1>
+                                   return <ul className="slides"> <li className='col-md-3' key={movie.id}><NavLink to={`/${movie.id}`}><img src={`https://image.tmdb.org/t/p/original${movie.image}`} alt={movie.title}></img></NavLink></li>
+                                   <li className="col-md-4 col-centered"><NavLink to={`/${movie.id}`}><h1 className='maintitle'>{movie.title}</h1>
                                    <h3 className='maintitle'>Year: {movie.year.substring(0,4)}</h3></NavLink></li></ul>
                                 })}
                         </div>
