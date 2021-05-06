c33b51d116f7bb4f3610506016992ea5ea1b6d83
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-05-06 15:44:38 -0500

    setup inital REST api files to store users

---
+++ API/app.js
@@ -0,0 +1,29 @@
+'use strict'
+const express = require('express');
+const db = require('./models');
+const cors = require('cors');
+
+const app = express();
+
+// creates port variable
+const PORT = process.env.PORT || 5000;
+
+// Enable All CORS Requests
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+(async () => {
+    await db.sequelize.sync();
+    try {
+      await db.sequelize.authenticate();
+      console.log('Connection to the database successful!');
+    } catch (error) {
+      console.error('Error connecting to the database: ', error);
+    }
+  })();
+  
+  app.listen(PORT, () => {
+    console.log(`Express server is listening on port ${PORT}`);
+  });
