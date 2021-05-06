36f3b1cead2ed9c66217fd4a0c9827775db777da
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-05-06 09:36:10 -0500

    Added buttons to search

--- my-app/src/Components/SearchResults.jsx
+++ my-app/src/Components/SearchResults.jsx
@@ -24,10 +24,10 @@ const SearchResults = () => {
                     <div className="col-md-12">
                         <div className="slider">
                         <h2>Search Results: {value.searchResults.length}</h2>
-                            <Carousel responsive={responsive}>
+                            <Carousel responsive={responsive} showDots={true}>
                                 {value.searchResults.map(movie => {
-                                   return <ul className="slides"> <li className='col-md-12 col-centered' key={movie.id}><NavLink to={`/${movie.id}`}><img src={movie.image ? `https://image.tmdb.org/t/p/original${movie.image}` : '/1.jpg'} alt={movie.title}></img></NavLink></li>
-                                   <li className="col-md-12 col-centered"><NavLink to={`/${movie.id}`}><h1 className='maintitle'>{movie.title}</h1>
+                                   return <ul className="slides"> <li className='col-md-12 search-col-centered' key={movie.id}><NavLink to={`/${movie.id}`}><img src={movie.image ? `https://image.tmdb.org/t/p/original${movie.image}` : '/1.jpg'} alt={movie.title}></img></NavLink></li>
+                                   <li className="col-md-12 search-col-centered"><NavLink to={`/${movie.id}`}><h1 className='maintitle'>{movie.title.length > 35 ? `${movie.title.substring(0,35)}...` : movie.title}</h1>
                                    <h3 className='maintitle'>Year: {movie.year ? movie.year.substring(0,4) : 'No Year Available'}</h3></NavLink></li></ul>
                                 })}
                             </Carousel>
